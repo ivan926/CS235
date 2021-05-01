44d339f31060c68d51498141a6cd43222c481e98
Ivan Arriola <navis462@gmail.com>
2021-05-01 00:40:25 -0600

    Commited all files within vectortemp directory

---
+++ vectorTemp/vector.cpp
@@ -0,0 +1,27 @@
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    string userSays = "";
+
+cout << "hello world";
+
+cout << endl;
+
+cout << "Please insert something here please: ";
+
+
+
+cin >> userSays;
+
+cout << endl;
+
+cout << "This should print out the users input " + userSays;
+
+cout << endl;
+
+}
